d08c1443b98ab642835a6424dac3af79f54415ed
Amit Mondal <52355355+AmitMondal22@users.noreply.github.com>
2022-03-23 19:57:16 +0530

    main file
    main file

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+     <div><p>hello world</p></div>
+     <div> <h1> hello amit  </h1> </div>
+    </div>
+  )
+}
